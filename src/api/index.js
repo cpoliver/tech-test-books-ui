55b6c5cc80eb59b41d7aeefd750fd68ddab2d27f
Charles P. Oliver <cpoliver@gmail.com>
2017-06-20 14:52:58 +0200

    Fix query param function to drop undefined keys

--- src/api/index.js
+++ src/api/index.js
@@ -1,10 +1,12 @@
 import axios from 'axios';
-import { join, map, pipe, toPairs } from 'ramda';
+import { any, isNil, join, map, pipe, reject, toPairs } from 'ramda';
 
 const SERVER_URL = 'http://localhost:5000'; // TODO: Make dynamic & document how to change
 
-const stringifyParams = ({ itemsPerPage, page, sort, filter }) => {
-  return pipe(toPairs, map(join('=')), join('&'))({
+const objectToQueryString = pipe(toPairs, reject(any(isNil)), map(join('=')), join('&'));
+
+const stringifyParams = ({ itemsPerPage, page, sort = {}, filter = {} }) => {
+  return objectToQueryString({
     itemsPerPage,
     page,
     sort: JSON.stringify(sort),
@@ -12,12 +14,12 @@ const stringifyParams = ({ itemsPerPage, page, sort, filter }) => {
   });
 };
 
-export const fetchBooks = ({ itemsPerPage, page, sort = {}, filter = {} }) => () => {
+export const fetchBooks = ({ itemsPerPage, page, sort, filter }) => () => {
   const params = stringifyParams({ itemsPerPage, page, sort, filter});
   return axios.get(`${SERVER_URL}/books?${params}`);
 };
 
-export const fetchTotal = ({ filter }) => {
+export const fetchTotal = ({ filter }) => () => {
   const params = stringifyParams({ filter });
   return axios.get(`${SERVER_URL}/books/count?${params}`);
 };
