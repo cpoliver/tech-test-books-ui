194c33deefaa85e327410f04b060749379d626b5
Charles P. Oliver <cpoliver@gmail.com>
2017-06-20 20:39:50 +0200

    Reset page to 1 when books change

--- src/reducers/bookListReducer.js
+++ src/reducers/bookListReducer.js
@@ -1,4 +1,4 @@
-import { evolve, flip, merge, mergeAll } from 'ramda';
+import { assocPath, evolve, flip, merge, mergeAll } from 'ramda';
 
 import { FETCH_BOOKS, FETCH_BOOKS_COMPLETED, FETCH_BOOKS_ERRORED, UPDATED_SEARCH_PARAMS_RECEIVED } from '../actions/types';
 
@@ -15,17 +15,25 @@ const initState = {
   error: {}
 };
 
+const onCompleteState = {
+  isFetching: false,
+  error: {}
+};
+
+const resetPage = assocPath(['searchParams', 'page'], 1);
+
 const booksReducer = (state = initState, action = {}) => {
   const { type, payload = {} } = action;
 
   const actions = {
     [FETCH_BOOKS]: merge(state, { isFetching: true }),
-    [FETCH_BOOKS_COMPLETED]: mergeAll([state, { isFetching: false, error: {} }, payload]),
+    [FETCH_BOOKS_COMPLETED]: mergeAll([resetPage(state), onCompleteState, payload]),
     [FETCH_BOOKS_ERRORED]: merge(state, { isFetching: false, error: payload }),
     [UPDATED_SEARCH_PARAMS_RECEIVED]: evolve({ searchParams: flip(merge)(payload) })(state)
   };
 
-  return actions[type] || state;
+  const x = actions[type] || state;
+  return x;
 };
 
 export default booksReducer;
