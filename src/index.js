bd13879b970e98382d53660628476cb35f0c747a
Charles P. Oliver <cpoliver@gmail.com>
2017-06-20 12:49:06 +0200

    Add redux-saga and simple demo code

--- src/index.js
+++ src/index.js
@@ -2,17 +2,22 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import createSagaMiddleware from 'redux-saga';
 import thunk from 'redux-thunk';
 
 import App from './components/App';
 import rootReducer from './reducers';
 import registerServiceWorker from './registerServiceWorker';
 import { fetchBooks, fetchTotal } from './api';
+import { helloSaga } from './sagas'
 
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const sagas = createSagaMiddleware()
+const store = createStore(rootReducer, applyMiddleware(thunk, sagas));
+
+sagas.run(helloSaga);
 
 store.dispatch(fetchBooks({ page: 1, itemsPerPage: 8 }));
 store.dispatch(fetchTotal());
