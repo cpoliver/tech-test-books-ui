4cb6e69b1dd9a0e517b12735141c26ebc5890acb
Charles P. Oliver <cpoliver@gmail.com>
2017-06-20 04:00:40 +0200

    Improve book styling

--- src/components/Book/index.js
+++ src/components/Book/index.js
@@ -1,42 +1,34 @@
 import React from 'react';
-import { Col, Panel, Row } from 'react-bootstrap';
+import { Col, Image, Row } from 'react-bootstrap';
 
 import './book.css';
 
 const authorName = ({ firstName, surname }) => `${firstName} ${surname}`;
 const authorGender = ({ gender }) => gender.substring(0, 1).toUpperCase();
 
-const randomInt = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
-const coverSrc = (genre) => require(`../../../public/covers/${genre}/${randomInt(0, 9)}.jpg`);
-
-const header = (title, author, publishedOn) => (
-  <Row className="book__header">
-    <Col xs={12}>
-      <span className="book__title">{title}</span>
-    </Col>
-    <Col xs={12}>
-      <span className="book__author">
-        by {authorName(author)} ({authorGender(author)})
-      </span>
-    </Col>
-    <Col xs={12}>
-      <span className="book__published">
-        published: {publishedOn}
-      </span>
-    </Col>
-  </Row>
-);
+// stupid way of getting a "random" image that's always the same for any given book
+const index = (publishedOn) => new Date(publishedOn).getTime().toString().split('')[3];
+const coverSrc = (genre, publishedOn) => require(`../../../public/covers/${genre}/${index(publishedOn)}.jpg`);
 
 const Book = ({ title, genre, author, publishedOn }) => (
-  <Col xs={12} sm={6} md={4} lg={3} className="app__book">
-    <Panel className="book" header={header(title, author, publishedOn)}>
-      <Row className="book__detail">
-        <Col xs={12}>
-          <img alt={title} src={coverSrc(genre)} />
-        </Col>
-      </Row>
-      <div className="book__offer">Special Offer</div>
-    </Panel>
+  <Col xs={12} sm={6} md={3} className="book">
+    <Row className="book__detail text-center">
+      <Col xs={12}>
+        <Image alt={title} src={coverSrc(genre, publishedOn)} className="center-block" responsive />
+      </Col>
+      <Col xs={12}>
+        <span className="book__title">{title}</span>
+      </Col>
+      <Col xs={12}>
+        <span className="book__by">by </span>
+        <span className="book__author">{authorName(author)} ({authorGender(author)})</span>
+      </Col>
+      <Col xs={12}>
+        <span className="book__published">{publishedOn}</span>
+        &nbsp;<small>&bull;</small>&nbsp;
+        <span className="book__genre">{genre}</span>
+      </Col>
+    </Row>
   </Col>
 );
 
