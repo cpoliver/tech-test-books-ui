2df0d49322ed22245a77f2e62f63af9023050c2d
Charles P. Oliver <cpoliver@gmail.com>
2017-06-22 20:20:00 +0200

    Add debounce to filter changes

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,9 +1,13 @@
+import { delay } from 'redux-saga';
 import { all, call, fork, put, select, takeLatest } from 'redux-saga/effects';
+import { has } from 'ramda';
 
 import { fetchBooks, fetchTotal } from '../api';
 import { queryStringSelector } from '../selectors';
 import { FETCH_BOOKS, FETCH_BOOKS_COMPLETED, FETCH_BOOKS_ERRORED,
          UPDATE_SEARCH_PARAMS, UPDATED_SEARCH_PARAMS_RECEIVED } from '../actions/types';
+
+const DEBOUNCE_DELAY = 1000;
 
 function* fetchBookList() {
   try {
@@ -24,7 +28,10 @@ function* fetchBookList() {
 }
 
 function* updateSearchParams({ payload }) {
+  const isFilterChange = has('filter', payload);
+
   yield put({ type: UPDATED_SEARCH_PARAMS_RECEIVED, payload });
+  yield call(delay, isFilterChange ? DEBOUNCE_DELAY : 0);
   yield put({ type: FETCH_BOOKS });
 }
 
