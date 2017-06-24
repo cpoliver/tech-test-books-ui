39043a2a9b1e7c6f43190d9f521ff463aff503fe
Charles P. Oliver <cpoliver@gmail.com>
2017-06-25 00:13:12 +0200

    Refresh books after adding/deleting from admin panel

--- src/sagas/admin.js
+++ src/sagas/admin.js
@@ -3,7 +3,9 @@ import { call, put, select } from 'redux-saga/effects';
 import { addBooks as tryAddBooks, deleteAllBooks as tryDeleteAllBooks } from '../api';
 import { totalToAddSelector } from '../selectors';
 import {
-  ADD_BOOKS_COMPLETED, ADD_BOOKS_ERRORED, DELETE_ALL_BOOKS_COMPLETED, DELETE_ALL_BOOKS_ERRORED,
+  ADD_BOOKS_COMPLETED, ADD_BOOKS_ERRORED,
+  DELETE_ALL_BOOKS_COMPLETED, DELETE_ALL_BOOKS_ERRORED,
+  FETCH_BOOKS
 } from '../actions/types';
 
 export function* addBooks() {
@@ -12,6 +14,7 @@ export function* addBooks() {
 
     yield call(tryAddBooks, totalToAdd);
     yield put({ type: ADD_BOOKS_COMPLETED });
+    yield put({ type: FETCH_BOOKS });
   } catch(error) {
     yield put({ type: ADD_BOOKS_ERRORED, payload: error });
   }
@@ -21,6 +24,7 @@ export function* deleteAllBooks() {
   try {
     yield call(tryDeleteAllBooks);
     yield put({ type: DELETE_ALL_BOOKS_COMPLETED });
+    yield put({ type: FETCH_BOOKS });
 } catch(error) {
     yield put({ type: DELETE_ALL_BOOKS_ERRORED, payload: error });
   }
