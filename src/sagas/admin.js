5a135957f6a9e9a0612fbac02cea554741a9c39e
Charles P. Oliver <cpoliver@gmail.com>
2017-06-24 23:10:18 +0200

    Add admin saga

---
+++ src/sagas/admin.js
@@ -0,0 +1,27 @@
+import { call, put, select } from 'redux-saga/effects';
+
+import { addBooks as tryAddBooks, deleteAllBooks as tryDeleteAllBooks } from '../api';
+import { totalToAddSelector } from '../selectors';
+import {
+  ADD_BOOKS_COMPLETED, ADD_BOOKS_ERRORED, DELETE_ALL_BOOKS_COMPLETED, DELETE_ALL_BOOKS_ERRORED,
+} from '../actions/types';
+
+export function* addBooks() {
+  try {
+    const totalToAdd = yield select(totalToAddSelector);
+
+    yield call(tryAddBooks, totalToAdd);
+    yield put({ type: ADD_BOOKS_COMPLETED });
+  } catch(error) {
+    yield put({ type: ADD_BOOKS_ERRORED, payload: error });
+  }
+}
+
+export function* deleteAllBooks() {
+  try {
+    yield call(tryDeleteAllBooks);
+    yield put({ type: DELETE_ALL_BOOKS_COMPLETED });
+} catch(error) {
+    yield put({ type: DELETE_ALL_BOOKS_ERRORED, payload: error });
+  }
+}
