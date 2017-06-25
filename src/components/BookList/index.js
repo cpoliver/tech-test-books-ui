cbf9dbb15fc39d8ad28d086a99515b27dab1377e
Charles P. Oliver <cpoliver@gmail.com>
2017-06-25 13:55:23 +0200

    Restyle pagination

--- src/components/BookList/index.js
+++ src/components/BookList/index.js
@@ -5,15 +5,17 @@ import { Col, Pagination, Row } from 'react-bootstrap';
 import Book from '../Book';
 import { bookShape, searchParamsType } from '../../lib/types';
 
+import './book-list.css';
+
 const BookList = ({ books, searchParams, totalPages, updateSearchParams }) => {
   const { page } = searchParams;
 
   return (
-    <Row>
-      <Row>
+    <Row className="book-list">
+      <Row className="book-list__grid">
         {books.map((book) => <Book key={book._id} {...book} />)}
       </Row>
-      <Row>
+      <Row className="book-list__pagination">
         <Col xs={12} className="text-center">
           <Pagination prev next first last ellipsis boundaryLinks
             items={totalPages}
